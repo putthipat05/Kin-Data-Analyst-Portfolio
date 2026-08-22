d9d253214cc9435beb3b16f64098b0065f1b180a
KIn <putthipat05@gmail.com>
2026-08-22 19:19:46 +0800

    Update and rename Readme.md to SQL.Query1.sql

--- SQL.Query1.sql
+++ SQL.Query1.sql
@@ -1,7 +1,7 @@
 International students
 
 ## Business Tasks
-Analyst how growth international students had come to study at Taiwan in this 5 years
+Analyst how growth of international students number had increased study at Taiwan in this 5 years
 
 ## Data Set
 - The number of students from 1 Jan 2021 to 1 Jan 1 2025 from(https://www.studyintaiwan.org/why-taiwan/key-numbers/chart1?q=eT0yMDI1JnNvcnRieT0wJmx2PUImcHJnPWZvcmVpZ24lMkNvdmVyc2VhJTJDY2hpbmE#table)
